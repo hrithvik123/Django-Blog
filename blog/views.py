c6c6883ef253011128c92e48977d0759ce3a959c
hrithvik123 <gadhiyahrithvik@gmail.com>
2021-02-26 23:30:42 +0530

    First Commit

---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post #import database from models file.
+
+
+# Create your views here.
+def home(request):
+    #context dictionary to pass as arguments to html page
+    context = {
+        'posts': Post.objects.all() #query all post in database
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    #pass a dictionary dynamically
+    return render(request, 'blog/about.html', {'title': 'About'})
